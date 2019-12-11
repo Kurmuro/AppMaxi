a0333baa38a9bdd510807e16aefd9f3cfe034833
Kurmuro <55317081+Kurmuro@users.noreply.github.com>
2019-12-11 23:33:05 +0100

    Datenbank Add und View hinzugefügt

---
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+
+    public static final String DATABASE_NAME = "user.db";
+    public static final String TABLE_NAME = "user_table";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "FIRSTNAME";
+    public static final String COL3 = "LASTNAME";
+    public static final String COL4 = "BOATTYPE";
+    public static final String COL5 = "YARDSTICK";
+
+
+
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " FIRSTNAME TEXT, LASTNAME TEXT, BOATTYPE TEXT, YARDSTICK INTEGER)";
+        db.execSQL(createTable);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean AddData(String firstname, String lastname, String boattype, int yardstick){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,firstname);
+        contentValues.put(COL3,lastname);
+        contentValues.put(COL4,boattype);
+        contentValues.put(COL5,yardstick);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Cursor showData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return data;
+    }
+}
