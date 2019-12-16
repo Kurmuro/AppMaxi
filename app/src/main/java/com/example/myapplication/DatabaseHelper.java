d54ed3f20e6800137b58c7962388573f267bb470
Kurmuro <55317081+Kurmuro@users.noreply.github.com>
2019-12-16 21:54:31 +0100

    Regatta

--- app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -31,7 +31,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY, " +
                 " FIRSTNAME TEXT, LASTNAME TEXT, BOATTYPE TEXT, YARDSTICK INTEGER)";
         db.execSQL(createTable);
 
